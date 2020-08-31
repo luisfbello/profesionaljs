7edf6a51920f5133f1c882336323a7971b8b677f
luisfbello <luisfbellogonzalez@gmail.com>
2020-08-31 16:24:05 -0500

    Se agrego live server

---
+++ assests/mediaPlayer.js
@@ -0,0 +1,17 @@
+
+function MediaPlayer(config){
+    this.media = config.el;
+
+}
+
+MediaPlayer.prototype.play = function() {
+
+if (this.media.paused) {
+    this.media.play();
+}else{
+    this.media.pause();
+}
+
+}
+
+export default MediaPlayer
